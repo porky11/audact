6b9ed983c9219a0448728b225319d30a5e21a33d
Fabio U. Krapohl <Krapohl.f@gmx.de>
2022-12-26 00:46:57 +0100

    Allow combining waves

--- src/waves.rs
+++ src/waves.rs
@@ -1,9 +1,29 @@
 use rand::random;
 
 /// Trait to represent possible wave functions
-pub trait Wave {
+pub trait Wave: Sized {
     /// Calculates the wave
     fn calculate(&self, t: f32) -> f32;
+
+    /// Mix waves using a specified ratio.
+    fn mix<A: Wave, B: Wave>(self, a: A, b: B) -> MixWave<A, B, Self> {
+        MixWave { a, b, ratio: self }
+    }
+
+    /// Multiply waves.
+    fn multiply<W: Wave>(self, other: W) -> MultiplyWave<Self, W> {
+        MultiplyWave { a: self, b: other }
+    }
+
+    /// Reverse the wave.
+    fn reverse(self) -> ReverseWave<Self> {
+        ReverseWave(self)
+    }
+
+    /// Flip the wave.
+    fn flip(self) -> FlipWave<Self> {
+        FlipWave(self)
+    }
 }
 
 impl Wave for fn(f32) -> f32 {
@@ -56,5 +76,49 @@ pub struct NoiseWave;
 impl Wave for NoiseWave {
     fn calculate(&self, _: f32) -> f32 {
         random::<f32>() * 2.0 - 1.0
+    }
+}
+
+/// Mixes waves using a specified ratio.
+pub struct MixWave<A: Wave, B: Wave, R: Wave> {
+    a: A,
+    b: B,
+    ratio: R,
+}
+
+impl<A: Wave, B: Wave, R: Wave> Wave for MixWave<A, B, R> {
+    fn calculate(&self, t: f32) -> f32 {
+        let ratio = self.ratio.calculate(t) / 2.0 + 0.5;
+        self.a.calculate(t) * ratio + self.b.calculate(t) * (1.0 - ratio)
+    }
+}
+
+/// Multipies waves.
+pub struct MultiplyWave<A: Wave, B: Wave> {
+    a: A,
+    b: B,
+}
+
+impl<A: Wave, B: Wave> Wave for MultiplyWave<A, B> {
+    fn calculate(&self, t: f32) -> f32 {
+        self.a.calculate(t) * self.b.calculate(t)
+    }
+}
+
+/// Reverses a wave.
+pub struct ReverseWave<W: Wave>(W);
+
+impl<W: Wave> Wave for ReverseWave<W> {
+    fn calculate(&self, t: f32) -> f32 {
+        self.0.calculate(1.0 - t)
+    }
+}
+
+/// Flips a wave.
+pub struct FlipWave<W: Wave>(W);
+
+impl<W: Wave> Wave for FlipWave<W> {
+    fn calculate(&self, t: f32) -> f32 {
+        -self.0.calculate(1.0 - t)
     }
 }
